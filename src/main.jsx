df85cc992d91ec50105882b0d28b1ce4daa84f41
João <contato@joaolds.xyz.br>
2026-08-14 16:39:53 -0300

    style: load realtime states

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './functional.css'
+import './realtime.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
